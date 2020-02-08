e18d9a1d9b506cc3fd97e65c303ea876ab9ddc3a
Irm <idriswan@gmail.com>
2020-02-08 22:59:38 +0530

    Updates

---
+++ precache-manifest.38473589abd2ad828a3a39abea4db967.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fa94a978be8246a356d",
+    "url": "/static/css/main.a1129f8c.chunk.css"
+  },
+  {
+    "revision": "3fa94a978be8246a356d",
+    "url": "/static/js/main.3fa94a97.chunk.js"
+  },
+  {
+    "revision": "63a8f196648076aa1c3d",
+    "url": "/static/js/1.63a8f196.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb28973ab9f167e0d6cc00dff4fcd824",
+    "url": "/index.html"
+  }
+];
