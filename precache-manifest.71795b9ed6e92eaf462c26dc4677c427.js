d00c6a8d68dc6a9b592a456f7326edee40ebe629
Irm <idriswan@gmail.com>
2020-02-08 19:38:53 +0530

    Portfolio

---
+++ precache-manifest.71795b9ed6e92eaf462c26dc4677c427.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10144f18ec36336c20a8",
+    "url": "/static/css/main.28f49dae.chunk.css"
+  },
+  {
+    "revision": "10144f18ec36336c20a8",
+    "url": "/static/js/main.10144f18.chunk.js"
+  },
+  {
+    "revision": "63a8f196648076aa1c3d",
+    "url": "/static/js/1.63a8f196.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39ad505351703c84f2d1c39c6b1ba472",
+    "url": "/index.html"
+  }
+];
