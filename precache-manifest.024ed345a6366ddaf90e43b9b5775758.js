215efb2f0955c7f28313802d7a58359119445bc4
Irm <idrishan1996@gmail.com>
2021-08-22 13:06:52 +0530

    Portfolio Update 2021

---
+++ precache-manifest.024ed345a6366ddaf90e43b9b5775758.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e768775070af14daf09c9ab898f5ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "18b3cf596596028c86f5",
+    "url": "./static/css/2.8edc603e.chunk.css"
+  },
+  {
+    "revision": "1585381747e2d9dd8d3f",
+    "url": "./static/css/main.5853e446.chunk.css"
+  },
+  {
+    "revision": "18b3cf596596028c86f5",
+    "url": "./static/js/2.daaa87de.chunk.js"
+  },
+  {
+    "revision": "9efecc6e366a89e935d3722c67df4853",
+    "url": "./static/js/2.daaa87de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1585381747e2d9dd8d3f",
+    "url": "./static/js/main.4529af53.chunk.js"
+  },
+  {
+    "revision": "2207813b094fb70c64c4",
+    "url": "./static/js/runtime-main.70050e21.js"
+  },
+  {
+    "revision": "b76c93b01f7250bec817f546e3d9a92a",
+    "url": "./static/media/Web Developer.b76c93b0.pdf"
+  }
+]);
