df16f4f3ac9564803ed681154d9ac8c5adc0618a
Irm <idrishan1996@gmail.com>
2022-03-31 21:44:58 +0530

    Revert "Mar 31 2022"
    This reverts commit d4f54797a64697d1dc856837c191c0f1ed80db47.

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import LapIcon from './../images/appleIcons/laptop.png'
-import CallWhiteIcon from './../images/appleIcons/callWhite.png'
 export default class About extends Component {
   render() {
     let resumeData = this.props.resumeData;
@@ -11,12 +9,12 @@ export default class About extends Component {
           </div>
 
           <div className="nine columns main-col">
-            <div className='aboutBox'><h2>About Me</h2><img src={LapIcon} alt='lap' width={70} /></div>
+            <h2>About Me</h2>
             <p>{resumeData.aboutme}</p>
 
             <div className="row">
               <div className="columns contact-details">
-                <div className='aboutBox'><h2>Contact Details</h2><img src={CallWhiteIcon} alt='lap' width={70} /></div>
+                <h2>Contact Details</h2>
                 <p className="address">
                   <span>{resumeData.name}</span>
                   <br />
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
-import { Carousel } from "antd";
+import { Modal, Carousel, Tabs, Image, Row, Col } from "antd";
+import ReactPlayer from "react-player";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import "antd/dist/antd.css";
-import Popup from "../componentFeatures/Popup";
-import { settings } from "../utility";
+import { FileImageOutlined, VideoCameraOutlined } from "@ant-design/icons";
 
 export default function Portfolio(props) {
   let resumeData = props.resumeData;
+  const { TabPane } = Tabs;
   const [poupState, setPoupState] = useState(false);
-  const [position, setPosition] = useState(0);
+  const [position, setPosition] = useState(0)
   const [portfolio, setPortfolio] = useState({});
   const handlePopUp = (item) => {
     setPoupState(true);
@@ -20,23 +22,160 @@ export default function Portfolio(props) {
 
   const handleCancel = () => {
     setPoupState(false);
-    setTimeout(() => {
-      window.scrollTo(0, position);
-    }, 0);
+    setTimeout(()=>{
+      window.scrollTo(0, position)
+    },0)
   };
   const renderPopup = () => {
     return (
-      <Popup
-        width={1000}
-        portfolio={portfolio}
-        isOpen={poupState}
-        handleOk={handleOk}
+      <Modal
+        title={portfolio?.detailName || portfolio.name}
+        visible={poupState}
+        onOk={handleOk}
         footer={null}
-        handleCancel={handleCancel}
-      />
+        onCancel={handleCancel}
+        width={1000}
+      >
+        <Row>
+          <Col
+            span={portfolio.demoVideo ? 8 : 6}
+            xs={24}
+            sm={24}
+            lg={portfolio.demoVideo ? 8 : 6}
+          >
+            {/* <Carousel autoplay dots={{className:'dotsSlider'}} dotPosition={'right'}> */}
+            {portfolio.demoVideo && portfolio.imgurl ? (
+              <>
+                <Tabs defaultActiveKey='2'>
+                  <TabPane
+                    tab={
+                      <span>
+                        <FileImageOutlined />
+                        Images
+                      </span>
+                    }
+                    key='1'
+                  >
+                    <Image
+                      src={`${portfolio.imgurl}`}
+                      alt={`${portfolio.imgurl}`}
+                      className='item-img'
+                    />
+                  </TabPane>
+                  <TabPane
+                    tab={
+                      <span>
+                        <VideoCameraOutlined />
+                        Video
+                      </span>
+                    }
+                    key='2'
+                  >
+                    <ReactPlayer
+                      className='react-player fixed-bottom'
+                      url={portfolio.demoVideo}
+                      width='100%'
+                      height='100%'
+                      controls={true}
+                    />
+                  </TabPane>
+                </Tabs>
+              </>
+            ) : portfolio.demoVideo ? (
+              <ReactPlayer
+                className='react-player fixed-bottom'
+                url={portfolio.demoVideo}
+                width='100%'
+                height='100%'
+                controls={true}
+              />
+            ) : (
+              <Image
+                src={`${portfolio.imgurl}`}
+                alt={`${portfolio.imgurl}`}
+                className='item-img'
+              />
+            )}
+            {/* </Carousel> */}
+          </Col>
+          <Col
+            span={portfolio.demoVideo ? 16 : 18}
+            xs={24}
+            sm={24}
+            lg={portfolio.demoVideo ? 16 : 18}
+            className='popupDesc'
+          >
+            <p>{portfolio?.description}</p>
+            {portfolio.web && (
+              <>
+                <Router>
+                  Demo Link:{" "}
+                  <Link to={{ pathname: portfolio.web }} target='_blank'>
+                    {portfolio.web}
+                  </Link>
+                </Router>
+              </>
+            )}
+            {portfolio?.responsiblities && (
+              <div className='responsiblities'>
+                <h5>
+                  <u>Responsiblities: -</u>
+                </h5>
+                <ul>
+                  {portfolio.responsiblities.map((response, index) => {
+                    return <li key={index}>{response}</li>;
+                  })}
+                </ul>
+              </div>
+            )}
+          </Col>
+        </Row>
+      </Modal>
     );
   };
-  
+  const settings = {
+    dots: { className: "mainCarouselDots" },
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: true,
+    draggable: true,
+    variableWidth: true,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          dots: false,
+          slidesToScroll: 2,
+          arrows: false,
+          centerMode: true,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          dots: false,
+          centerMode: true,
+          arrows: false,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
     <>
